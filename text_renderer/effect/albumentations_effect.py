07e6b9caf689f6c450ba5e9d1a60d0de24995ac6
igor <igor@scanbot.io>
2025-12-15 11:25:41 +0100

    More blur

--- text_renderer/effect/albumentations_effect.py
+++ text_renderer/effect/albumentations_effect.py
@@ -379,8 +379,8 @@ class TLR(AlbumentationsEffect):
     def __init__(self, p=1.0):
         transform = A.OneOf(
             [
-                A.GaussianBlur(blur_limit=(1, 5), p=1.0),
-                A.MotionBlur(blur_limit=(3, 7), p=1.0),
+                A.GaussianBlur(blur_limit=(1, 9), p=1.0),
+                A.MotionBlur(blur_limit=(3, 11), p=1.0),
                 A.Lambda(image=per_channel_coarse_dropout, p=1.0),
                 A.InvertImg(p=1.0),
                 A.NoOp(p=1.0),
